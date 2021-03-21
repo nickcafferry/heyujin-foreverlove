3ee0f42f22a6379d48ff0072cf7c006b592b230b
Wei Mei <56918345+nickcafferry@users.noreply.github.com>
2021-03-21 14:42:09 +0800

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+sys.path.insert(0, os.path.abspath("../../manim/"))
+
+project = u'Foreverlove-for-She-Heyujin'
+copyright = u'- Wei MEI (Nick Cafferry)'
+author = u'Wei MEI'
+
+
+version = '1.0'
+release = '1.0'
+
+extensions = [
+    'sphinx.ext.todo',
+    'sphinx.ext.githubpages',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.autodoc', 
+    'sphinx.ext.coverage',
+    'sphinx.ext.napoleon',
+    'sphinx_rtd_theme',
+    'sphinx.ext.graphviz',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.inheritance_diagram'
+    
+]
+
+autoclass_content = 'both'
+mathjax_path = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"
+
+templates_path = ['_templates']
+source_suffix = '.rst'
+master_doc = 'README'
+language = 'english'
+html_search_language = 'Chinese'
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'remain']
+pygments_style = 'default'
+
+html_static_path = ['assets']
+html_theme = 'haiku'
+html_logo = 'GCC.svg'
+html_favicon = 'GCC.svg'
+html_theme_options = {
+    'linkcolor': "red",
+    "external_links": [
+        {"url": "https://psspred.readthedocs.io/en/latest/README.html?badge=latest", "name": "PSSpred"}
+    ],
+    "github_url": "https://github.com/nickcafferry/PSSpred",
+
+}
+html_sidebars = {
+   '**': ['globaltoc.html', 'sourcelink.html', 'searchbox.html'],
+   'using/windows': ['windowssidebar.html', 'searchbox.html','playground.html'],
+}
